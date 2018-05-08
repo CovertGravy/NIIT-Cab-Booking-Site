6246c848b1d71fa71ba5f083554336f6ccb430b0
Ashish S <s.cricket.ashish@gmail.com>
2018-05-08 13:14:33 +0530

    destination v1.0

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -34,6 +34,38 @@ angular.module('myApp').controller('HomeController', function ($scope) {
 
       let map = new google.maps.Map(canvas, options);
       let geocoder = new google.maps.Geocoder;
+      let bounds = new google.maps.LatLngBounds();         
+
+      let search = document.getElementById('search');
+      let autocomplete = new google.maps.places.Autocomplete(search);
+
+      autocomplete.bindTo('bounds', map);
+
+      autocomplete.addListener('place_changed', function(){
+        let place = autocomplete.getPlace();
+
+        if(!place.geometry) {
+          window.alert(`No details available for ${place.name}`);
+        }
+
+        if(place.geometry.viewport) {
+          map.fitBounds(place.geometry.viewport);
+          //map.setCenter(place.geometry.location);
+          console.log(place);
+
+          let destination = new google.maps.Marker({
+            position: place.geometry.location,
+            map: map
+          });
+          let bound2 = new google.maps.LatLng(destination.position.lat(), destination.position.lng());
+
+          bounds.extend(bound2);
+
+          map.fitBounds(bounds);
+          map.panToBounds(bounds);
+          
+        }
+      });
 
       let marker = new google.maps.Marker({
         position: coords,
@@ -41,11 +73,16 @@ angular.module('myApp').controller('HomeController', function ($scope) {
         draggable: true
       });
 
+      let bound1 = new google.maps.LatLng(marker.position.lat(), marker.position.lng());
+      bounds.extend(bound1);
+
       // reverse geocoder for current navigator position
       geocoder.geocode({'location': coords}, function(results, status){
         if(status === 'OK'){
           if(results[0]){
             document.getElementById('pos-input').value = results[0].formatted_address;
+            // materialize textarea resize on init
+            M.textareaAutoResize(document.getElementById('pos-input'));
             console.log(results[0]);
           } else {
             window.alert('No results found!');
@@ -55,6 +92,7 @@ angular.module('myApp').controller('HomeController', function ($scope) {
         }
       });
 
+      // dragend event with reverse geocoder
       google.maps.event.addListener(marker, 'dragend', function(e){
         let newPos = {
           lat: e.latLng.lat(),
